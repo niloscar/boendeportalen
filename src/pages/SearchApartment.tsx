409be69c6bc683eacb2ebc574e8896a802e544e6
Erica <erica.friberg89@gmail.com>
2026-05-15 18:36:10 +0200

    set up mock data for different fetch calls

--- src/pages/SearchApartment.tsx
+++ src/pages/SearchApartment.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import ApartmentList from '../components/ApartmentList.tsx'
 import ApartmentFilter from '../components/ApartmentFilter.tsx'
-import type { Apartment } from "../types/Apartment.ts";
+import type { Apartment, Rent, Available, FullData } from "../types/Apartment.ts";
 
 const SearchApartment = () => {
+    const [fullData, setFullData] = useState<FullData[]>([]);
+
     const [apartments, setApartments] = useState<Apartment[]>([
         {
             id: 1,
@@ -25,7 +27,47 @@ const SearchApartment = () => {
             district: "Östermalm",
             description: "En trevlig enrummare mitt i stan!"
         }
-    ])
+    ]);
+    const [rent, setRent] = useState<Rent[]>([
+        {
+            id: 1,
+            apartment_id: 1,
+            rent: 8000,
+            start_date: "2026-01-01",
+            end_date: null
+        },
+        {
+            id: 2,
+            apartment_id: 2,
+            rent: 13000,
+            start_date: "2026-01-01",
+            end_date: null
+        },
+    ]);
+    const [availableFrom, setAvailableFrom] = useState<Available[]>([{
+        id: 1,
+        apartment_id: 1,
+        renter_id: 1,
+        start_date: "2025-01-01",
+        end_date: "2026-01-01"
+    },
+    {
+        id: 2,
+        apartment_id: 1,
+        renter_id: 14,
+        start_date: "2026-02-02",
+        end_date: null
+    },
+    {
+        id: 3,
+        apartment_id: 2,
+        renter_id: 2,
+        start_date: "2025-01-01",
+        end_date: null
+    }
+    ]);
+
+
     return (
         <div>
             <h1>Lediga lägenheter</h1>
--- src/types/Apartment.ts
+++ src/types/Apartment.ts
@@ -2,7 +2,31 @@ export interface Apartment {
     id: number, street: string, postcode: number, city: string, area: string, rooms: string, district: string, description: string
 }
 
-export type ApartmentProps = {
-    apartments: Apartment[]
-    apartment: Apartment
+export interface Rent {
+    id: number,
+    apartment_id: number,
+    rent: number,
+    start_date: string,
+    end_date: string | null
+}
+
+export interface Available {
+    id: number,
+    apartment_id: number,
+    renter_id: number,
+    start_date: string,
+    end_date: string | null
+}
+
+export interface FullData {
+    id: number,
+    street: string,
+    postcode: number,
+    city: string,
+    area: number,
+    rooms: string,
+    district: string,
+    description: string,
+    rent: number,
+    available: string
 }
