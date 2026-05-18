3b812f3353048514f1de3013a1d7f965f242c6dc
Viktor Lindqvist <lindqvistcs@gmail.com>
2026-05-18 22:52:55 +0200

    Added temporary logout button & logged in as text on frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
+import Auth from './pages/Auth'
+import { signOut } from './lib/supabase'
+import useAuth from './hooks/useAuth'
 
 function App() {
+    const { user } = useAuth()
+
     return (
         <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<div>Detta är en placeholder. Gå till <a href="/login" className="text-blue-500 hover:underline">Inloggning</a></div>} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<div>Detta är en placeholder. Gå till <a href="/auth" className="text-blue-500 hover:underline">Inloggning</a>
+                {user && <div className="mt-4">
+                    <div className="text-green-600">Inloggad som {user?.email}</div>
+                    <button onClick={async () => { await signOut(); window.location.reload(); }} className="py-2 px-4 bg-red-600 text-white rounded cursor-pointer hover:bg-red-700">Logga ut</button>
+                </div>
+                }
+            </div>} />
+
         </Routes>
     );
 }
