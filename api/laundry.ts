87501a38b6deb5fb70bf5aa5bc7d4e8cf66cd336
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-05-18 11:39:17 +0200

    Moved components into subfolder and updated references

--- api/laundry.ts
+++ api/laundry.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Booking, Timeslot } from "../src/components/Calendar";
+import type { Booking, Timeslot } from "../src/components/LaundryPage/Calendar";
 
 // Types -----------------------------
 
--- src/pages/Laundry.tsx
+++ src/pages/Laundry.tsx
@@ -1,7 +1,7 @@
-import Calendar from "../components/Calendar";
+import Calendar from "../components/LaundryPage/Calendar";
 import { useEffect, useState } from "react";
 import { fetchLaundrySlots, fetchBookedSlots } from "../../api/laundry";
-import type { Timeslot, Booking } from "../components/Calendar";
+import type { Timeslot, Booking } from "../components/LaundryPage/Calendar";
 
 export default function Laundry() {
     const [timeslots, setTimeslots] = useState<Timeslot[]>([]);
