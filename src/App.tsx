939b9be5a869b86e876506a83d711ecbc99353e9
Viktor Lindqvist <lindqvistcs@gmail.com>
2026-05-19 04:06:13 +0200

    Added routes, route guards and more useAuth functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import Auth from './pages/Auth'
 import { signOut } from './lib/supabase'
 import useAuth from './hooks/useAuth'
+// Import route guards here, like PrivateRoute and AdminRoute, if you want to use them in this file.
+import { PublicOnlyRoute } from './routing'
 
 function App() {
-    const { user } = useAuth()
+    // Add profile in here if you want to send profile info to route guards, like AdminRoute.
+    const { user, loading } = useAuth()
 
     return (
         <Routes>
-            <Route path="/auth" element={<Auth />} />
+            <Route path="/auth" element={<PublicOnlyRoute user={user} loading={loading}><Auth /></PublicOnlyRoute>} />
+            {/* <Route path="/minasidor" element={<PrivateRoute user={user} loading={loading}><div>Detta är en privat sida.</div></PrivateRoute>} /> */}
+            {/* <Route path="/admin" element={<AdminRoute user={user} loading={loading} profile={profile}><div>Detta är en admin-sida.</div></AdminRoute>} /> */}
             <Route path="/" element={<div>Detta är en placeholder. Gå till <a href="/auth" className="text-blue-500 hover:underline">Inloggning</a>
                 {user && <div className="mt-4">
                     <div className="text-green-600">Inloggad som {user?.email}</div>
@@ -16,7 +21,6 @@ function App() {
                 </div>
                 }
             </div>} />
-
         </Routes>
     );
 }
