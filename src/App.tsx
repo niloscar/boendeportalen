7a6bb9a96812c43cd094e7ad6b7f8aa7cea35765
Viktor Lindqvist <lindqvistcs@gmail.com>
2026-05-22 18:19:35 +0200

    Added justify-center to main wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
-            <main className="flex-1">
+            <main className="flex-1 flex justify-center items-start">
                 <Routes>
                     <Route path="/auth" element={<PublicOnlyRoute user={user} loading={loading}><Auth /></PublicOnlyRoute>} />
                     <Route path="/apartment" element={<SearchApartment />} />
