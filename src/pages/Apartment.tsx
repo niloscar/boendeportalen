95123876f612a24b92c6f437cf58a0d7c6f4eab5
Erica <erica.friberg89@gmail.com>
2026-05-23 22:46:48 +0200

    hiding buttons if not logged in

--- src/pages/Apartment.tsx
+++ src/pages/Apartment.tsx
@@ -5,13 +5,14 @@ import ApartmentList from '../components/SearchApartmentPage/ApartmentList.tsx';
 import type { ApartmentData, Detail } from '../types/apartment.ts';
 import ApartmentSignUp from '../components/SearchApartmentPage/ApartmentSignUp.tsx';
 import { SignUpCalls } from '../hooks/SignUpCalls.tsx';
+import { useSession } from '../hooks/useAuth.ts';
 
 const Apartment = () => {
     const location = useLocation();
+    const session = useSession();
     const state = location.state as
         | { apartment: ApartmentData; details: Detail[] }
         | undefined;
-
     const apartment: ApartmentData | null = state?.apartment ?? null;
     const details: Detail[] | null = state?.details ?? null;
     if (!apartment || !details) {
@@ -37,7 +38,7 @@ const Apartment = () => {
         <section className="flex flex-col gap-6 p-6 max-w-3xl">
             <div className="flex justify-between">
                 <h1 className="text-5xl">{apartment.street}</h1>
-                <ApartmentSignUp error={error} loading={loading} saved={saved} applied={applied} deleted={deleted} deleteSignUp={deleteSignUp} signUp={signUp} />
+                {session && <ApartmentSignUp error={error} loading={loading} saved={saved} applied={applied} deleted={deleted} deleteSignUp={deleteSignUp} signUp={signUp} />}
             </div>
             <ImageCarousel images={apartment.images} size="large" />
             <h2 className="text-2xl">Om bostaden</h2>
@@ -48,7 +49,10 @@ const Apartment = () => {
             <ul className="max-w-md">
                 <ApartmentList variant="ul" items={details} />
             </ul>
-            <ApartmentSignUp error={error} loading={loading} saved={saved} applied={applied} deleted={deleted} deleteSignUp={deleteSignUp} signUp={signUp} />
+            {session ?
+                <ApartmentSignUp error={error} loading={loading} saved={saved} applied={applied} deleted={deleted} deleteSignUp={deleteSignUp} signUp={signUp} /> :
+                <div>Du måste vara inloggad för att anmäla intressse. Vänligen <a href="/auth" className="text-blue-500 hover:underline">logga in</a> eller registrera ett konto.</div>
+            }
         </section>
     )
 }
