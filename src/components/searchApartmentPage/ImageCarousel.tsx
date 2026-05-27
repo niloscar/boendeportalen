8e353ec08d1033cefa39f524cdde221315ff40e6
Erica <erica.friberg89@gmail.com>
2026-05-27 09:23:17 +0200

    added phosphor icons for carousel

--- src/components/searchApartmentPage/ImageCarousel.tsx
+++ src/components/searchApartmentPage/ImageCarousel.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import type React from 'react';
 import type { ApartmentImagesProp } from "../../types/apartment.ts";
+import { CaretRightIcon, CaretLeftIcon } from "@phosphor-icons/react"
+
 
 const ImageCarousel = ({ images, size }: ApartmentImagesProp) => {
     const [nextIndex, setNextIndex] = useState(0);
@@ -27,8 +29,8 @@ const ImageCarousel = ({ images, size }: ApartmentImagesProp) => {
         images.length > 0 ?
             <div className="relative m-auto w-full">
                 <img src={images[nextIndex].url} className={`w-full object-fill rounded-2xl h-50 ${imageSize} m-auto`} alt={images[nextIndex].description} />
-                <button className={`hover:bg-neutral-200 absolute top-[40%] cursor-pointer text-green-500 rounded-l text-2xl p-4`} onClick={(e) => back(e)}>❮</button>
-                <button className={`hover:bg-neutral-200 absolute top-[40%] right-0 cursor-pointer text-green-500 rounded-r text-2xl p-4`} onClick={(e) => forward(e)}>❯</button>
+                <button className={`hover:bg-neutral-200 absolute top-[40%] text-green-500 rounded-l text-2xl p-4`} onClick={(e) => back(e)}><CaretLeftIcon weight="bold" size={32}/></button>
+                <button className={`hover:bg-neutral-200 absolute top-[40%] right-0 text-green-500 rounded-r text-2xl p-4`} onClick={(e) => forward(e)}><CaretRightIcon weight="bold" size={32}/></button>
             </div>
             :
             <div>Bilder ej tillgänliga</div>
