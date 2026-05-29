fe62d7b05fb3c029c78af229fb9be89272642a9d
Erica <erica.friberg89@gmail.com>
2026-05-29 11:19:53 +0200

    CSS changes to homepage to make it mobile friendly and with a cohesive design

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -15,20 +15,22 @@ export default function Landing({ profileName, loading, loadError, visibleFeatur
     return (
         <>
             <img src={userImageUrl} className="h-48 w-full object-cover rounded-2xl" />
-            <h1 className="text-3xl font-bold">Välkommen till Boende<span className="text-green-500">Portalen</span>, {profileName}!</h1>
-            <section className='rounded-2xl border border-neutral-200 bg-white p-6 shadow-md sm:p-8 w-full text-neutral-700'>
-                <div className="p-2">
-                    <p>Här i BoendePortalen hittar du allt som rör ditt boende hos oss.</p>
-                </div>
-                <div className="p-2">
-                    <p>Du hittar dina uppgifter under <Link to='/minasidor' className="text-green-500">Mina sidor</Link>. Här kan du uppdatera din personliga information, och se status på dina förfrågningar. </p>
+            <section className='rounded-2xl border border-neutral-200 p-6 shadow-md sm:p-8 w-full text-neutral-700 bg-gradient-to-br from-white via-white to-green-50 '>
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="text-3xl font-bold sm:text-4xl">Välkommen till Boende<span className="text-green-500">Portalen</span>, {profileName}!</h1>
+                        <p className="text-neutral-700">Här i BoendePortalen hittar du allt som rör ditt boende hos oss.</p>
+                    </div>
+                    <div className="text-neutral-700">
+                        <p>Du hittar dina uppgifter under <Link to='/minasidor' className="text-green-500">Mina sidor</Link>. Här kan du uppdatera din personliga information, och se status på dina förfrågningar. </p>
+                    </div>
                 </div>
             </section>
             {(!loading && !loadError && visibleFeatures.length > 0) && (
-                <section className="grid grid-cols-2 gap-6 w-full">
+                <section className="grid lg:grid-cols-2 gap-6 w-full">
                     {visibleFeatures.map((feature) => (
                         <Link to={featureRoutes[feature.slug] ?? '/'} key={feature.id}>
-                            <section className='rounded-2xl border border-neutral-200 bg-white p-6 shadow-md sm:p-8 w-full flex gap-4' >
+                            <article className='flex gap-4 rounded-2xl border border-neutral-200 bg-gradient-to-br from-white via-white to-green-50 p-6 shadow-md p-6 sm:p-8 w-full ' >
                                 <div className='self-center'>
                                     {feature.slug === 'tvattstuga' &&
                                         <WashingMachineIcon size={32} className="text-green-500" />
@@ -50,7 +52,7 @@ export default function Landing({ profileName, loading, loadError, visibleFeatur
                                     <h3 className="text-lg text-neutral-900 font-semibold">{feature.name}</h3>
                                     <p className="text-neutral-700">{feature.description}</p>
                                 </div>
-                            </section>
+                            </article>
                         </Link>
                     ))}
                 </section>
