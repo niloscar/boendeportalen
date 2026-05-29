beb604604585fe0cf983e0192f10e935f6d678ca
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-05-29 18:12:29 +0200

    Moved calendar CSS into separate file

--- src/components/bookings/LaundryRoomCalendarView.tsx
+++ src/components/bookings/LaundryRoomCalendarView.tsx
@@ -4,6 +4,7 @@ import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
 import svLocale from "@fullcalendar/core/locales/sv";
 import type { LaundryRoomCalendarPropsBig } from "../../types/booking";
+import './CalendarView.css'
 
 export default function LaundryRoomCalendarView({
     calendarRef,
