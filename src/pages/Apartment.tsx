1c99b61d54930fd19465da911fc098bfc4d1109e
Erica <erica.friberg89@gmail.com>
2026-05-29 13:59:10 +0200

    added loading and error handling

--- src/pages/Apartment.tsx
+++ src/pages/Apartment.tsx
@@ -10,6 +10,8 @@ import ApartmentSignUp from '../components/searchapartment/ApartmentSignUp.tsx';
 import { useApartmentSignUp } from '../hooks/useApartmentSignUp.ts';
 import { useSession } from '../hooks/useAuth.ts';
 import { getDetails, fetchApartments } from '../utils/apartments.ts'
+import Skeleton from '@mui/material/Skeleton';
+
 
 const Apartment = () => {
     const { state } = useLocation();
@@ -21,15 +23,28 @@ const Apartment = () => {
     const [details, setDetails] = useState<Detail[] | null>(
         state?.details ?? null
     );
+    const [apartmentLoading, setApartementLoading] = useState(false);
+    const [apartmentError, setApartmentError] = useState('');
     const finalDayToApply = details && details.find(d => d.title == 'Sista ansökningsdag');
     const finalDateToApply = finalDayToApply && new Date(finalDayToApply.content);
     useEffect(() => {
+        if (apartmentLoading) return;
         if (!state?.apartment && apartmentId) {
             const loadApartment = async () => {
-                const data = await fetchApartments(apartmentId);
-                if (data) {
-                    setApartment(data);
-                    setDetails(getDetails(data));
+                try {
+                    setApartmentError('');
+                    setApartementLoading(true);
+                    const data = await fetchApartments(apartmentId);
+                    if (data) {
+                        setApartment(data);
+                        setDetails(getDetails(data));
+                    }
+                } catch (error) {
+                    if (error instanceof Error) {
+                        setApartmentError(error.message);
+                    }
+                } finally {
+                    setApartementLoading(false);
                 }
             };
 
@@ -46,7 +61,7 @@ const Apartment = () => {
         totalApplications
     } = useApartmentSignUp({ apartment });
 
-    if (!apartment || !details) {
+    if (!apartment || !details || apartmentError != '') {
         return (
             <section className="flex flex-col items-center gap-6 p-6 w-full">
                 <p>Kunde inte hämta information om lägenheten. Vänligen gå tillbaka och försök igen.</p>
@@ -65,25 +80,31 @@ const Apartment = () => {
                 <span>/</span>
                 <span className="text-neutral-900">{apartment.street} {apartment.house_number}</span>
             </div>
-            <article className="flex flex-col gap-6 rounded-2xl border border-neutral-200 bg-white p-6 shadow-md sm:p-8">
-                <h1 className="text-5xl">{apartment.street} {apartment.house_number}</h1>
-                <ImageCarousel images={apartment.images} size="large" rounded={true} />
-                <h2 className="text-2xl">Om bostaden</h2>
-                <p>{apartment.description}</p>
-                <h2 className="text-2xl">Visning</h2>
-                <p>Visning sker två veckor innan sista ansökningsdag. Du kommer att få en kallelse. Vänligen notera att visningstiden ej går att boka om. Om du inte kan delta i person går det bra att skicka en ersättare med giltlig legitimation. </p>
-                <h2 className="text-2xl">Detaljer</h2>
-                <ul className="grid md:grid-cols-2 md:gap-y-1">
-                    <ApartmentList variant="ul" items={details} />
-                </ul>
-                {session ? 
-                finalDateToApply && finalDateToApply >= new Date() ?
-                    <ApartmentSignUp error={error} loading={loading} applied={applied} deleteSignUp={deleteSignUp} signUp={signUp} totalApplications={totalApplications} /> :
-                    <div>Det går inte längre att ansöka denna lägenhet.</div>
-                    :
-                    <div>Du måste vara inloggad för att anmäla intressse. Vänligen <Link to="/inloggning" className="text-blue-500 hover:underline">logga in</Link> eller registrera ett konto.</div>
-                }
-            </article>
+            {apartmentLoading ?
+                <div className="flex flex-col gap-6 rounded-2xl border border-neutral-200 bg-white p-6 shadow-md sm:p-8">
+                    <Skeleton variant="rounded" height="1000px" />
+                </div>
+                :
+                <article className="flex flex-col gap-6 rounded-2xl border border-neutral-200 bg-white p-6 shadow-md sm:p-8">
+                    <h1 className="text-5xl">{apartment.street} {apartment.house_number}</h1>
+                    <ImageCarousel images={apartment.images} size="large" rounded={true} />
+                    <h2 className="text-2xl">Om bostaden</h2>
+                    <p>{apartment.description}</p>
+                    <h2 className="text-2xl">Visning</h2>
+                    <p>Visning sker två veckor innan sista ansökningsdag. Du kommer att få en kallelse. Vänligen notera att visningstiden ej går att boka om. Om du inte kan delta i person går det bra att skicka en ersättare med giltlig legitimation. </p>
+                    <h2 className="text-2xl">Detaljer</h2>
+                    <ul className="grid md:grid-cols-2 md:gap-y-1">
+                        <ApartmentList variant="ul" items={details} />
+                    </ul>
+                    {session ?
+                        finalDateToApply && finalDateToApply >= new Date() ?
+                            <ApartmentSignUp error={error} loading={loading} applied={applied} deleteSignUp={deleteSignUp} signUp={signUp} totalApplications={totalApplications} /> :
+                            <div>Det går inte längre att ansöka denna lägenhet.</div>
+                        :
+                        <div>Du måste vara inloggad för att anmäla intressse. Vänligen <Link to="/inloggning" className="text-blue-500 hover:underline">logga in</Link> eller registrera ett konto.</div>
+                    }
+                </article>
+            }
         </section>
     )
 }
